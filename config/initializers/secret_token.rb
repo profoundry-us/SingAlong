2d8ba510910a047e9a6156a201248e95aae024c2
Topher Fangio <fangiotophia@gmail.com>
2011-12-03 16:43:06 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SingAlong::Application.config.secret_token = '25c1d0b2dc27c7953ccbfea26b2437df00147824e764d9c1426f7a4504c74dc4ac3c57d9dff1c6eb431d6cfeb01cc2a3cac26436c536ea9ef5b08b67383b82b2'
